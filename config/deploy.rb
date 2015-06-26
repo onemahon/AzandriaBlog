47e2bc5a802bb1f30d8cbdd507b253f2be97c410
Joe Mahon <joe@muffin-2.local>
2015-06-26 04:01:22 -0400

    fix a couple little broken things

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock "3.4.0"
 
 set :application, "blaugh"
-set :repo_url, "git@bitbucket.org:onemahon/azandria-blaugh.git.git"
+set :repo_url, "git@bitbucket.org:onemahon/azandria-blaugh.git"
 
 set :deploy_to, "/var/www/#{fetch(:application)}"
 set :deploy_user, "rincewind"
@@ -12,7 +12,7 @@ set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rben
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
 
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :keep_releases, 5
