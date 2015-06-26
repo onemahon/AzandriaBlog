fd4ab527f48c170f69d4ffe6c6710105cb90641b
Joe Mahon <joe@muffin-2.local>
2015-06-26 15:11:41 -0400

    Update setup scripts

--- scripts/setup_chef.sh
+++ scripts/setup_chef.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
 # check for correct number of arguments
-if [ $# -ne 3 ]; then
-  echo "Usage: $0 <user> <ip> <port>"
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <ip> (ONLY RUN ON FIRST SETUP OF NEW MACHINE)"
   exit 1
 fi
 
 # set variables
-USER=$1
-IP=$2
-PORT=$3
-
+IP=$1
 
 # upload key for root
 ssh-copy-id -i ~/.ssh/id_rsa.pub root@$IP
@@ -20,22 +17,3 @@ cd ../config/chef && knife solo prepare root@$IP
 
 # execute the run list
 knife solo cook root@$IP
-
-# upload key for user
-ssh-copy-id -i ~/.ssh/id_rsa.pub -p $PORT $USER@$IP
-
-# upload app
-cd ../.. && cap production setup:all
-
-# restart nginx
-ssh -p $PORT -t $USER@$IP 'sudo service nginx restart'
-
-
-# This is already done, for but rincewind.
-# ssh-copy-id -i ~/.ssh/id_rsa.pub $USER@$IP -p $PORT
-# 
-# install chef
-# cd ../config/chef && bundle exec knife solo prepare $USER@$IP -p $PORT
-# 
-# execute the run list
-# bundle exec knife solo cook $USER@$IP -p $PORT
---
+++ scripts/update_chef.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# check for correct number of arguments
+if [ $# -ne 3 ]; then
+  echo "Usage: $0 <user> <ip> <port>"
+  exit 1
+fi
+
+# set variables
+USER=$1
+IP=$2
+PORT=$3
+
+# install chef
+knife solo prepare -p $PORT $USER@$IP
+
+# execute the run list
+knife solo cook -p $PORT $USER@$IP
+
+# upload key for user
+ssh-copy-id -i ~/.ssh/id_rsa.pub -p $PORT $USER@$IP
+
+# upload app
+cd ../.. && cap production setup:all
+
+# restart nginx
+ssh -p $PORT -t $USER@$IP 'sudo service nginx restart'
