6c27aaabf3ff09a080d28d32bf3b0dae5a0caea2
Joe Mahon <jmahon@wespire.com>
2015-09-22 18:12:38 -0400

    Add a couple todos for direction

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -4,24 +4,10 @@ ActiveAdmin.register Post do
   form do |f|
     f.inputs do
       f.input :title
+      # TODO: determine if html editor is necessary (as opposed to confusing)
       f.input :content, as: :html_editor
     end
 
     f.actions
   end
-
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
-
-
 end
