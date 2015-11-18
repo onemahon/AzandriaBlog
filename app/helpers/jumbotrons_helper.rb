b24032f6aec6289c193fc80652d694bec3475ef9
Joe Mahon <mahonjd@gmail.com>
2015-11-17 22:57:23 -0500

    quessttttioooonn marrrrrk

--- app/helpers/jumbotrons_helper.rb
+++ app/helpers/jumbotrons_helper.rb
@@ -1,6 +1,6 @@
 module JumbotronsHelper
   def jumbotron_text
-    if @jumbotron.quote_text_full
+    if @jumbotron.quote_text_full.present?
       @jumbotron.quote_text_full
     else
       @jumbotron.quote
